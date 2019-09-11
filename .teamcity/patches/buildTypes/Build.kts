20a9fd27ba8b9f48489b25ef21ece7570f3f7e2a
admin <admin@TeamCity>
2019-09-11 11:58:33 -0400

    TeamCity change in 'Simple Java Maven App (1)' project: runners of 'Build' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -22,6 +23,16 @@ changeBuildType(RelativeId("Build")) {
             maven {
                 goals = "clean package"
                 runnerArgs = "-Dmaven.test.failure.ignore=true -DskipTests"
+            }
+        }
+        insert(2) {
+            script {
+                name = "Coverity"
+                scriptContent = """
+                    pwd;
+                    cov-capture --project-dir ./;
+                    cov-analyze --dir idir --webapp-security --disable-fb --export-summaries false;
+                """.trimIndent()
             }
         }
     }
