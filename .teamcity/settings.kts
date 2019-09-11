7c893d2da28dbd4498932543e7e4be533bef4ddb
admin <admin@TeamCity>
2019-09-11 12:15:45 -0400

    TeamCity change in 'Simple Java Maven App (1)' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 /*
@@ -43,6 +44,18 @@ object Build : BuildType({
             enabled = false
             goals = "clean test"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        maven {
+            goals = "clean package"
+            runnerArgs = "-Dmaven.test.failure.ignore=true -DskipTests"
+        }
+        script {
+            name = "Coverity"
+            scriptContent = """
+                pwd;
+                cov-capture --project-dir ./;
+                cov-analyze --dir idir --webapp-security --disable-fb --export-summaries false;
+            """.trimIndent()
         }
     }
 
